1d24d542ab04ed82bfd1d1549f223d10e0cdefe5
maciejbilinski <maciej.bilinski.programming@gmail.com>
2022-12-30 21:07:47 +0100

    Arduino Keypad Program

---
+++ arduino/keypad.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad cKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char cKey = cKeypad.getKey();
+  
+  if (cKey){
+    Serial.println(cKey);
+  }
+}
